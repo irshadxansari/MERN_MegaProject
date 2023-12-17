0a9b6407812370d932a2d1e15583ff8140ef07bb
John Doe <aforball76@gmail.com>
2023-12-17 20:57:47 +0545

    util, response, error

--- Readme.md
+++ Readme.md
@@ -1,3 +1,5 @@
 # Major Backend Project
 
 This is a major backend project with Javascript
+
+- [Model link](https://app.eraser.io/workspace/YtPqZ1VogxGy1jzIDkzj?origin=share)
---
+++ src/db/index.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+const connectDB = async() => {
+    try {
+        const connectionInstant = await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+        console.log(`\n MongoDB connected sucessfully || DB_HOST: ${connectionInstant.connection.host}`);
+    } catch (error) {
+        console.log("MongoDB connection FAILED ", error);
+        process.exit(1)
+    }
+}
+
+export default connectDB
